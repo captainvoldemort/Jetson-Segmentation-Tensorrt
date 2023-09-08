173430437a3acc32400c2b2342c46c2570cb6648
Yash Kulkarni <85916446+yashreadytobox@users.noreply.github.com>
2023-09-08 09:42:10 +0530

    Update detection.py

--- detection.py
+++ detection.py
@@ -24,7 +24,7 @@ class YoloTRT():
         self.LEN_ALL_RESULT = 38001
         self.LEN_ONE_RESULT = 38
         self.yolo_version = yolo_ver
-        self.categories = ["bus_stop", "20_mph", "do_not_enter”, “do_not_stop”, “do_not_turn_l”, “do_not_turn_r”, “do_not_u_turn”, “enter_left_lane”, “green_light”, “left_right_lane”, “no_parking”, “parking”, “ped_crossing”, “ped_zebra_cross”, “railway_crossing”, “red_light”, “stop”, “t_intersection_l”, “traffic_light”, “u_turn”, “warning”, “yellow_light”]
+        self.categories = ["bus_stop", "20_mph", "do_not_enter", "do_not_stop", "do_not_turn_l", "do_not_turn_r", "do_not_u_turn", "enter_left_lane", "green_light", "left_right_lane", "no_parking", "parking", "ped_crossing", "ped_zebra_cross", "railway_crossing", "red_light", "stop", "t_intersection_l", "traffic_light", "u_turn", "warning", "yellow_light"]
         
         TRT_LOGGER = trt.Logger(trt.Logger.INFO)
 
